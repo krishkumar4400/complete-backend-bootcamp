36a0eecd2e45eb1cade02a1ccc66057aa796f1da
krishkumar4400 <krishkumarbgp2022@gmail.com>
2026-03-17 21:24:36 +0530

    Add authentication middleware

--- projects/04-ai-saas-platform/middleware/auth.middleware.js
+++ projects/04-ai-saas-platform/middleware/auth.middleware.js
@@ -1,30 +1,27 @@
-
-
-
-export async function authenticationMiddleware(req,res) {
-    try {
-        const tokenHeader = req.headers;
-        if(!tokenHeader) {
-            return next();
-        }
-
-        const payload = jwt.verify(tokenHeader, process.env.JWT_SECRET);
-
-        req.userId = payload.userId;
-        next();
-    } catch (error) {
-        console.error(error);
-        return next();
+export async function authenticationMiddleware(req, res) {
+  try {
+    const tokenHeader = req.headers;
+    if (!tokenHeader) {
+      return next();
     }
+
+    const payload = jwt.verify(tokenHeader, process.env.JWT_SECRET);
+
+    req.userId = payload.userId;
+    next();
+  } catch (error) {
+    console.error(error);
+    return next();
+  }
 }
 
-export async function isAuthenticated(req,res,next) {
-    if(!req.userId) {
-        return res.status(403).json({
-            message: "You are not logged in",
-            success: false 
-        });
-    }
+export async function isAuthenticated(req, res, next) {
+  if (!req.userId) {
+    return res.status(403).json({
+      message: "You are not logged in",
+      success: false,
+    });
+  }
 
-    next();
+  next();
 }
